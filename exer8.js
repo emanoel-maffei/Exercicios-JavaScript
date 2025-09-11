1339e7a5be5820e1737c25d543a13fb0685eee3f
emanoel-maffei <emanoel.maffei@escola.pr.gov.br>
2025-09-11 12:26:24 -0300

    Add files via upload

--- exer8.js
+++ exer8.js
@@ -6,7 +6,9 @@ let transporte = 300;   // Combustível, transporte público e etc.
 let lazer = 450;        // Passeios, cinema, etc.       
 let contas = 600;       // Luz, água, internet, telefone
 let educacao = 200;     // Mensalidade escolar, cursos
+let pSaude = 1100;
 
+console.log("\n")
 console.log("--- Despesas Familiares Registradas ---");
 console.log(`Moradia: ${moradia}`);
 console.log(`Alimentacao: ${alimentacao}`);
@@ -14,11 +16,12 @@ console.log(`Transporte: ${transporte}`);
 console.log(`Lazer: ${lazer}`);
 console.log(`Contas: ${contas}`);
 console.log(`Educacao: ${educacao}`);
+console.log(`As despesas com Plano de Saúde é: R$ ${pSaude}`);
 console.log("---------------------------------------");
 
 // --- Cálculo do Total de Despesas ---
 
-let totalDespesas = moradia + alimentacao + transporte + lazer + contas + educacao;
+let totalDespesas = moradia + alimentacao + transporte + lazer + contas + educacao + pSaude;
 
 console.log(`Total de Despesas: R$ ${totalDespesas}`)
 
@@ -26,4 +29,8 @@ console.log(`Total de Despesas: R$ ${totalDespesas}`)
 
 // Para cálcular a média, precisamos saber quantas "categorias" de despesas temos.
 // Neste exemplo, temos 6 categorias.
-let numeroDeCategorias = 6
+let numeroDeCategorias = 7
+let mediaDespesas = totalDespesas / numeroDeCategorias;
+
+console.log(`Média de Despesas por Categoria: R$ ${mediaDespesas.toFixed(2)}`) // .toFixed(2) formata para 2 casas decimais
+console.log("\n")
