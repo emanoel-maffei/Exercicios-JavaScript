06a2883f920be3306d61b326fa60c6363ae72d9b
emanoel-maffei <emanoel.maffei@escola.pr.gov.br>
2025-09-22 09:11:26 -0300

    Create exer13.js

---
+++ exer13.js
@@ -0,0 +1,10 @@
+/*
+    Criando um array
+    Crie um novo arquivo JS e, dentro dele , elabore um exemplo simpes de array. Seu código deve fazer o seguinte:
+    - Declare uma váriavel usando const ou let para armazenar uma lista que você gosta;
+    - Adicione pelo menos 4 elementos a sua lista na declaração;
+    - Acesse e imprima no terminal apenas o segundo elemento da sua lista;
+    - Adicione um novo elemento ao final do seu array;
+    - Imprima no terminal o array completo para confirmar se o novo item foi adicionado corretamente.
+    - Após finalisar, execute seu código no Node.js e veja o resultado.
+*/
